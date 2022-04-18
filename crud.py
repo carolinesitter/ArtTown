401b4a12929a4bd0fee56f76dca0b736c814f8a5
Caroline <carolinesitter24@gmail.com>
2022-04-18 13:41:50 -0500

    Established Bare Bones of What to Import for crud.py

---
+++ crud.py
@@ -0,0 +1,24 @@
+"""CRUD operations."""
+
+# from model import db, User, Movie, Rating, connect_to_db
+#will need to substitute class names for what we implement with our data
+
+
+# insert functions and query statements in order to:
+# - parse through our data
+# - create query statements 
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     from server import app
+#     connect_to_db(app)
